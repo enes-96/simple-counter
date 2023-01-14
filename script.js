84baba505d6d8a53666fb3cbd04ce125c203261a
Enes <seyit7enes@gmail.com>
2023-01-14 17:56:09 +0100

    done

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 "use strict";
 const displayValue = document.querySelector(".value");
-const increaseBtn = document.querySelector(".increase");
-const resetBtn = document.querySelector(".reset");
-const decreaseBtn = document.querySelector(".decrease");
 
 let counter = 0;
 
-increaseBtn.addEventListener("click", () => {
-  counter++;
-  displayValue.innerHTML = counter;
-  if (counter < 0) {
-    displayValue.style.color = "red";
-  } else if (counter > 0) {
-    displayValue.style.color = "blue";
-  } else {
-    displayValue.style.color = "black";
-  }
-});
+const increaseBtn = document
+  .querySelector(".increase")
+  .addEventListener("click", () => {
+    counter++;
+    displayValue.innerHTML = counter;
+    if (counter < 0) {
+      displayValue.style.color = "red";
+    } else if (counter > 0) {
+      displayValue.style.color = "blue";
+    } else {
+      displayValue.style.color = "black";
+    }
+  });
 
-resetBtn.addEventListener("click", () => {
-  counter = 0;
-  displayValue.innerHTML = counter;
-  displayValue.style.color = "black";
-});
-
-decreaseBtn.addEventListener("click", () => {
-  counter--;
-  displayValue.innerHTML = counter;
-  if (counter < 0) {
-    displayValue.style.color = "red";
-  } else if (counter > 0) {
-    displayValue.style.color = "blue";
-  } else {
+const resetBtn = document
+  .querySelector(".reset")
+  .addEventListener("click", () => {
+    counter = 0;
+    displayValue.innerHTML = counter;
     displayValue.style.color = "black";
-  }
-});
+  });
+
+const decreaseBtn = document
+  .querySelector(".decrease")
+  .addEventListener("click", () => {
+    counter--;
+    displayValue.innerHTML = counter;
+    if (counter < 0) {
+      displayValue.style.color = "red";
+    } else if (counter > 0) {
+      displayValue.style.color = "blue";
+    } else {
+      displayValue.style.color = "black";
+    }
+  });
